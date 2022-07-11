7720c784058395bd01a4002bd07b66245efe9b40
jheaps <jheaps@5J1ZDX2-C>
2022-07-11 10:58:05 -0600

    Fix sprite scaling

--- PlayerSprite.java
+++ PlayerSprite.java
@@ -71,7 +71,10 @@ public class PlayerSprite extends JComponent
 				int spriteScaleX = mapScaleX / 20;
 				int spriteScaleY = mapScaleY / 20;
 				spriteColors.add(c);
-				spritePixels.add(new Rectangle(xCoordinate+(i-(img.getWidth()/2))+((i-1)*(spriteScaleX/2)) - (mapScaleX/2),yCoordinate+(j-(img.getHeight()/2))+((j-1)*(spriteScaleY/2)) - (mapScaleY/2),spriteScaleX+1,spriteScaleY+1));
+				spritePixels.add(new Rectangle(xCoordinate+(i-(img.getWidth()/2))+((i-1)*(spriteScaleX/2)) - (mapScaleX/2),
+											   yCoordinate+(j-(img.getHeight()/2))+((j-1)*(spriteScaleY/2)) - (mapScaleY/2),
+											   spriteScaleX+1-((spriteScaleX)/2),
+											   spriteScaleY+1-((spriteScaleY)/2)));
 				
 			}
 			
